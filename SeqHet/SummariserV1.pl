c58da78784bcf7b7433639382ab8129b20310b98
GSeed <seedgeorge@gmail.com>
2014-06-19 17:12:54 +0100

    developing the run summariser

--- SeqHet/SummariserV1.pl
+++ SeqHet/SummariserV1.pl
@@ -8,15 +8,15 @@ use Data::Dumper;
 use DateTime;
 use File::Path qw(make_path);
 
-# THis script is designed to summarise the results from a discrete set of SeqHet runs. 
-# It produces a summary table, with columns being individual sample scores and the rows different pathways
+# This script is designed to summarise the results from a discrete set of SeqHet runs. 
+# It produces a summary table, rows being individual pathways, and the columns as mean score, standard deviation
 
-# Section 1  - directory opening and file scanning
-print "Directory to scan: ";
+# ---------- Section 1  - directory opening and file scanning
+print "Run to summarise? ";
 chomp (my $directory = <STDIN>);
 print "\n";
 
-my $fulldir = "Data/$directory/Trim";
+my $fulldir = "Results/$directory";
 print "Trying to open ","$fulldir:\n";
 
 opendir DIR, $fulldir or die "This directory doesn't exist!";
@@ -29,3 +29,21 @@ foreach my $file (@files) {
 print "\n";
 
 @files = @files[2.. $#files];
+
+# ---------- Section 2  - sum the score for each pathway
+my %pathwaytotals;
+
+foreach my $file (@files) {
+	open (FH,"$fulldir/$file");
+	while (my $line = <FH>) {
+		chomp $line;
+		if ($line =~ /(.+)\,(\d+$)/) {
+			my $path = $1;
+			my $score = $2;
+			$pathwaytotals{$path} += $score;
+		}
+	}
+	close (FH);
+}
+
+# ---------- Section 3  - generate the mean and std dev for each pathway
