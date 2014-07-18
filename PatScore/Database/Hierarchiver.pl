dc5e7dcb9a1c793f3de78c1751547144c788f28c
GSeed <seedgeorge@gmail.com>
2014-07-18 17:46:58 +0100

    hierarchy script basic
    argh

---
+++ PatScore/Database/Hierarchiver.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use utf8;
+use 5.010;
+use Data::Dumper;
+use DateTime;
+use File::Path qw(make_path);
+use List::Util qw(sum);
+
+my $filehandle = "Pathway Hierarchy.txt";
+
+
+my @pathways;
+open (FH,"$filehandle");
+while (my $line = <FH>) {
+	if ($line =~ /\(Pathway\)/){
+		push (@pathways, $line);
+	}
+}
+
+open (OUTPUT, ">Pathways.txt");
+foreach my $thing (@pathways) {
+	print OUTPUT "$thing\n";
+}
